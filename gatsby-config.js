64f3bd5e598c526d03e7d38a8b9be26a34361a60
Ariel Rodriguez <arielrodriguez@ariels-mbp.lan>
2022-04-08 14:06:35 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    title: `pinSupply`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "gSy8_NyO0a-WSg5pyxK-1yPY9KMB5KJe92bHI9qHDBc",
+      "spaceId": "zo9jnchrfrsd"
+    }
+  }, "gatsby-plugin-styled-components"]
+};
