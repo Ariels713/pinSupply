b690c1b33e8dbebc2244216c421f7ec24a28f60b
Ariel Rodriguez <arielrodriguez@ariels-mbp.lan>
2022-04-11 16:30:27 -0400

    destructuring data

--- components/collections/collectionsHeader/index.js
+++ components/collections/collectionsHeader/index.js
@@ -1,7 +1,4 @@
 import React from 'react'
-import { GatsbyImage, getImage } from 'gatsby-plugin-image'
-import { Link } from 'gatsby'
-
 import {
   Wrapper,
   CardGrid,
@@ -10,6 +7,7 @@ import {
   CollectionTitle,
 } from './styles.js'
 import { createGlobalStyle } from 'styled-components'
+import CollectionCard from '../collectionsCard/index.js'
 
 function CollectionsHeader({ data }) {
   return (
@@ -20,30 +18,7 @@ function CollectionsHeader({ data }) {
           Collections
         </h3>
       </div>
-      <Wrapper>
-        <CardGrid>
-          {data.map((collection) => {
-            return (
-              <>
-                <ImageWrapper key={collection.id}>
-                  <Link to={collection.collectionTitle.slug}>
-                    <GatsbyImage
-                      image={getImage(collection.collectionTitle.bannerImage)}
-                      alt={collection.collectionTitle.alt}
-                    />
-                  </Link>
-                  <CollectionTitle>
-                    {collection.collectionTitle.name}
-                  </CollectionTitle>
-                  <CollectionDescription>
-                    {collection.collectionTitle.description}
-                  </CollectionDescription>
-                </ImageWrapper>
-              </>
-            )
-          })}
-        </CardGrid>
-      </Wrapper>
+      <CollectionCard data={data} />
     </>
   )
 }
